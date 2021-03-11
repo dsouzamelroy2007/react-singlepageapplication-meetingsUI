bd4cdd1e3717bf64ef83ab41d09f406650a4b8c4
Melroy Dsouza <dsouzamelroy2007@gmail.com>
2021-03-12 02:03:36 +0530

    setting up firebase and connecting to firebase for user data

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Home from "./Home";
 import Welcome from "./Welcome";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Navigation from './Navigation';
 import Login from './Login';
 
 import { Router } from "@reach/router";
+import firebase from "./Firebase";
 
 function App() {
 
-  const[user, setUser] = useState("Mel");
+  const[user, setUser] = useState("");
+  const ref = firebase.database().ref('user');
+
+  useEffect(() => {
+    //const ref = firebase.database().ref('user');
+
+    ref.on('value', snapshot => {
+      let FBUser = snapshot.val();
+      setUser(FBUser);
+    })
+  },[ref]);
+
   return (
     <div>
         <Navigation user={user}/>
---
+++ src/Firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJg6AMtocZz-YKtElz8Zf-4BJ90vGzHOs",
+    authDomain: "react-meetings-spa-e4a2e.firebaseapp.com",
+    databaseURL: "https://react-meetings-spa-e4a2e-default-rtdb.firebaseio.com",
+    projectId: "react-meetings-spa-e4a2e",
+    storageBucket: "react-meetings-spa-e4a2e.appspot.com",
+    messagingSenderId: "187932931951",
+    appId: "1:187932931951:web:0e553bc96b3423d2aeda9a",
+    measurementId: "G-V2V71HT657"
+  };
+  firebase.initializeApp(firebaseConfig);
+  export const provider = new firebase.auth.GoogleAuthProvider();
+  export const auth = firebase.auth();
+
+  export default firebase;
